3fd5819b67bc3c844560e561b84419adc624880a
M V V S Manoj Kumar <mvvsmanojkumar@gmail.com>
2024-09-24 21:15:23 +0530

    updated for zen3

--- machine_specific/run_and_collect_data_zen3.sh
+++ machine_specific/run_and_collect_data_zen3.sh
@@ -24,7 +24,7 @@ for str in "${strings[@]}"; do
       FOLDER_NAME="${OUTPUT_DIR}/${DATE}/${MACHINE}_multiple_roofline/${TYPE}_itr${ITRS}_sleep${SLEEP_TIME}_160"
       
       # Remove the folder if it exists
-      echo $PASSWORD | sudo -S rm -rf ${FOLDER_NAME}
+      # echo $PASSWORD | sudo -S rm -rf ${FOLDER_NAME}
       
       # Create the folder
       mkdir -p $FOLDER_NAME
@@ -32,10 +32,10 @@ for str in "${strings[@]}"; do
       SOURCE_DIR="kernels/a-roofline-model-of-energy-ubenchmarks/cpu/amd/zen3/ryzen7_5845_${TYPE}"
       
       # Run commands
-      echo $PASSWORD | sudo -S cpupower frequency-info > ${FOLDER_NAME}/cpupower_frequency_info.txt 2>&1
-      echo $PASSWORD | sudo -S likwid-features -l -c N > ${FOLDER_NAME}/likwid_features.txt 2>&1
+      # echo $PASSWORD | sudo -S cpupower frequency-info > ${FOLDER_NAME}/cpupower_frequency_info.txt 2>&1
+      # echo $PASSWORD | sudo -S likwid-features -l -c N > ${FOLDER_NAME}/likwid_features.txt 2>&1
       
-      ./utils_roofline.py --source_dir ${SOURCE_DIR} --output_dir ${FOLDER_NAME} --machine ${MACHINE} --build_dir "${DATE}_bin_test" --password ${PASSWORD} --iterations ${ITRS} --sleep_time ${SLEEP_TIME} --env_ITR 1  #> ${FOLDER_NAME}/log.txt 2>&1      
+      ./utils_roofline.py --source_dir ${SOURCE_DIR} --output_dir ${FOLDER_NAME} --machine ${MACHINE} --build_dir "${DATE}_bin_test" --password ${PASSWORD} --iterations ${ITRS} --sleep_time ${SLEEP_TIME} --env_ITR 1 --core #> ${FOLDER_NAME}/log.txt 2>&1      
     done
   done
 done
